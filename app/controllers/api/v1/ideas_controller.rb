553a4a031877c86b7f010268e38626cbee262c51
Pete1281 <30956172+Pete1281@users.noreply.github.com>
2017-12-13 15:18:17 -0600

    wrap IdeasController class in module

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -1,7 +1,9 @@
-class IdeasController < ApplicationController
-  def index
-    @ideas = Idea.all
-    render json: @ideas
+  module Api::V1
+  class IdeasController < ApplicationController
+    def index
+      @ideas = Idea.all
+      render json: @ideas
 
+    end
   end
 end
